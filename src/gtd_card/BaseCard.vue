8bd31469b14918df2d86d283dacdbe4db60b7472
Paul Nodwell <paulnodwell24@googlemail.com>
2025-08-20 15:02:23 +0200

    Beautification

--- src/gtd_card/BaseCard.vue
+++ src/gtd_card/BaseCard.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="gtd-card">
-    <div class="mod-card-section">
-      <h3 class="mod-card-title">{{ title }}</h3>
-      <div class="mod-scrollable" style="max-height: 350px;">
-        <slot />
-      </div>
-      <div style="display: flex; justify-content: flex-end; margin-top: 1rem;">
-        <button class="mod-cta" @click="onCreate">+ New</button>
-      </div>
+    <h3 class="gtd-card-title">{{ title }}</h3>
+    <div class="gtd-card-content mod-scrollable" style="max-height: 350px;">
+      <slot />
+    </div>
+    <div style="display: flex; justify-content: flex-end; margin-top: 1rem;">
+      <button class="mod-cta" @click="onCreate">+ New</button>
     </div>
   </div>
 </template>
